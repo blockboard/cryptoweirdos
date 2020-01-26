21c4217c925c7587277c94fe651f4484f29f95d7
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-01-26 14:33:11 +0000

    Connecting successfully to IPFS through Infura node

--- client/src/util/ipfs.js
+++ client/src/util/ipfs.js
@@ -1,4 +1,6 @@
 const ipfsClient = require('ipfs-http-client');
-const ipfs = ipfsClient('http://localhost:5001'); // (the default in Node.js)
+//const ipfs = ipfsClient('http://localhost:5001');
+const ipfs = ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
 
 export default ipfs;
