54addd1cc7bba095f449c222be589d2b18033598
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-01-28 08:54:04 +0000

    Add missing files

--- contracts/Cryptofaces.sol
+++ contracts/Cryptofaces.sol
@@ -4,6 +4,9 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 contract Cryptofaces is ERC721Full {
 
+
+    string internal tokenBaseURI = "https://ipfs.infura.io/ipfs/";
+
     constructor() ERC721Full("Cryptofaces", "Ṏ") public {
 
     }
