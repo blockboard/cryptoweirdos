a96cf80598b06186dc495ed218f4e94e95f83d5d
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-01-31 13:48:12 +0000

    Adding Mint and tokenOf Functions to CryptoFaces Contract

--- contracts/CryptoFaces.sol
+++ contracts/CryptoFaces.sol
@@ -17,6 +17,8 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 
 // Utils only
 import "./Strings.sol";
+import "openzeppelin-solidity/contracts/drafts/Counters.sol";
+import "openzeppelin-solidity/contracts/utils/Address.sol";
 
 /**
  * @title CF-V1
@@ -26,56 +28,30 @@ import "./Strings.sol";
 
 contract CryptoFaces is ERC721Full, AccessControl, Pausable {
     using SafeMath for uint256;
+    using Address for address;
+    using Counters for uint256;
 
     /*
      * Properties
      */
-
     string internal tokenBaseURI = "https://ipfs.infura.io/ipfs/";
 
-    // To begin from index 1
-    uint public nextFaceIndexToAssign = 100001;
+    // To begin from index 100001
+    uint256 private tokenId = 100001;
 
-    // Number of CFs minted
-    uint256 public totalFacesMinted;
+    // All tokens of owner
+    uint256[] private ownerTokens;
 
-    // For single face offer
-    struct Offer {
-        bool isForSale;
-        uint256 faceIndex;
-        address seller;
-        uint minValue;          // in ether
-        address onlySellTo;     // specify to sell only to a specific person
-    }
-
-    // For a bundle of faces offer
-    struct multiOffers {
-        bool areForSale;
-        uint256[] facesIndexes;
-        address seller;
-        uint minValue;
-        address onlySellTo;
-    }
-
-    struct Bid {
-        bool hasBid;
-        uint faceIndex;
-        address bidder;
-        uint value;
-    }
+    // Faces Exits
+    mapping(string => bool) faceExits;
 
     /*
      * Events
      */
-    event Minted(
-        uint256 indexed _tokenId,
-        address indexed _buyer
-    );
 
     /*
      * Modifiers
      */
-
 
     /*
      * Constructor
@@ -84,10 +60,20 @@ contract CryptoFaces is ERC721Full, AccessControl, Pausable {
 
     }
 
-    /*function mint(address _to, uint256 _tokenId) public onlyIfCryptoFacesArtists {
-        super._mint(_to, _tokenId);
+    function tokensOf(address _owner) public view returns (uint256[] memory _tokenIds) {
+        require(_owner != address(0));
 
+        _tokenIds = super._tokensOfOwner(owner);
 
-    }*/
+        return _tokenIds;
+    }
 
+    function mintFaceToken(string memory _tokenURI) public onlyIfCryptoFacesArtists {
+        require(!faceExits[_tokenURI]);
+
+        super._mint(msg.sender, tokenId);
+        super._setTokenURI(tokenId, _tokenURI);
+
+        tokenId = tokenId.add(1);
+    }
 }
--- test/Cryptofaces.test.js
+++ test/Cryptofaces.test.js
@@ -23,31 +23,95 @@ contract('CryptoFaces', (accounts) => {
         describe('Deployment', async () => {
             it('should deployed successfully', async () => {
                 const _address = contract.address;
-                console.log(_address);
+
                 assert.notEqual(_address, '');
                 assert.notEqual(_address, null);
                 assert.notEqual(_address, 0x0);
                 assert.notEqual(_address, undefined);
             });
+        });
 
+        describe('Metadata', async () => {
             it('should has a name', async () => {
                 const _name = await contract.name();
+
                 assert.equal(_name, name);
             });
 
             it('should has a symbol', async () => {
                 const _symbol = await contract.symbol();
+
                 assert.equal(_symbol, symbol);
             });
 
             it('should has owner', async () => {
                 const _owner = await contract.owner();
+
                 assert.equal(_owner, owner)
             });
+        });
 
-            /*it('should owner be from AccessControl Accounts', function () {
-                const _ownerRole = await contract.methods.hasRole(owner, )
-            });*/
+        describe('Mint', async () => {
+            it('should mint new token', async () => {
+                const _imageURI = 'QmdP14WWurNzKDPdHDjwS8QDbNdSQyySxTedKqHzKcReBb';
+                const _imageURI2 = 'QmdP14WWurNzKDPdHDjwS8QDbNdSQyySxTedKqHzKcRserb';
+
+                await contract.mintFaceToken(_imageURI, {
+                    from: owner
+                });
+                const _totalSupply = await contract.totalSupply();
+
+                assert.equal(_totalSupply, 1);
+            });
+
+            it('should mint another token', async () => {
+                const _imageURI = 'QmdP14WWurNzKDPdHDjwS8QDbNdSQyySxTedKqHzKcRserb';
+
+                await contract.mintFaceToken(_imageURI, {
+                    from: owner
+                });
+                const _totalSupply = await contract.totalSupply();
+
+                assert.equal(_totalSupply, 2);
+            });
+
+            it('should mint third token', async () => {
+                const _imageURI = 'QmdP14WWurNzKdfDPdHDjwS8QDbNdSQyySxTedKqHzKcRserb';
+
+                await contract.mintFaceToken(_imageURI, {
+                    from: owner
+                });
+                const _totalSupply = await contract.totalSupply();
+
+                assert.equal(_totalSupply, 3);
+            });
+
+            it('should output the balanceOf', async () => {
+                const _ownerBalance = await contract.balanceOf(owner);
+
+                assert.equal(_ownerBalance, 3);
+            });
+
+            it('should output second tokenId', async () => {
+                const _secondTokenId = await contract.tokenOfOwnerByIndex(owner, 2);
+
+                assert.equal(_secondTokenId, 100003);
+            });
+
+            it('should output tokens Ids for owner', async () => {
+                const _ownerTokens = await contract.tokensOf(owner);
+                const expectedOwnerTokens = [100001, 100002, 100003];
+                const outputOwnerTokens = [];
+
+                _ownerTokens.forEach( element => {
+                    const tokenId = element.words[0];
+                    outputOwnerTokens.push(tokenId);
+                });
+
+                //console.log(outputOwnerTokens);
+
+                assert.equal(outputOwnerTokens, outputOwnerTokens);
+            });
         })
     })
 });
