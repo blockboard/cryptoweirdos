70f316de2e56acb678ff26def051a3a68a087454
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-01-31 22:18:14 +0000

    Adding purchase function and event, TokenDetails, and mapping of tokenIdToTokenDetails

--- contracts/CryptoFaces.sol
+++ contracts/CryptoFaces.sol
@@ -18,6 +18,9 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol";
 // ERC721 MetadataMintable
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721MetadataMintable.sol";
 
+// ERC721 MetadataMintable
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Burnable.sol";
+
 // Utils only
 import "./Strings.sol";
 import "openzeppelin-solidity/contracts/drafts/Counters.sol";
@@ -34,28 +37,50 @@ ERC721Full,
 ERC721MetadataMintable,
 AccessControl,
 Pausable {
+
+    /*************
+     * Libraries *
+     *************/
     using SafeMath for uint256;
     using Address for address;
     using Counters for uint256;
 
-    /*
-     * Properties
-     */
+    /**************
+     * Properties *
+     **************/
+    // Base URI link for Faces Hashes (URI)
     string internal tokenBaseURI = "https://ipfs.infura.io/ipfs/";
 
     // To begin from index 100001
     uint256 private tokenId = 100001;
 
-    // All tokens of owner
-    string[] facesURI;
+    // All faces URIs
+    string[] tokenURIs;
 
     // Faces Exits
-    mapping(string => bool) faceExists;
+    mapping(string => bool) tokenURIExists;
 
-    /*
-     * Events
-     */
+    // Total wei been processed through the contract
+    uint256 public totalPurchaseValueInWei;
 
+    // Object for token details
+    struct TokenDetails{
+        // Identifiers
+        uint256 tokenId;    // the range e.g. 10000
+        // Config
+        address artistAccount;    // artists account
+        uint256 priceInWei;       // base price for edition, could be overridden by external contracts
+        string tokenURI;          // IPFS hash - see base URI
+        // Counters
+        uint256 totalPurchases;      // Total purchases
+    }
+
+    // _tokenId : EditionDetails
+    mapping(uint256 => TokenDetials) internal tokenIdToTokenDetails;
+
+    /**********
+     * Events *
+     **********/
     // Emitted on every mint
     event Minted(
         uint256 indexed _tokenId,
@@ -63,15 +88,29 @@ Pausable {
         address indexed _buyer
     );
 
-    /*
-     * Modifiers
-     */
+    // Emitted on purchases from within this contract
+    event Purchase(
+        uint256 indexed _tokenId,
+        address indexed _buyer,
+        uint256 _priceInWei
+    );
 
-    /*
-     * Constructor
-     */
-    constructor() public payable ERC721Full("CryptoFaces", "CF") {}
+    /*************
+     * Modifiers *
+     *************/
+    modifier onlyValidTokenId(uint256 _tokenId) {
+        require(exists(_tokenId), "Token ID does not exist");
+        _;
+    }
 
+    /***************
+     * Constructor *
+     ***************/
+    constructor() public payable ERC721Full("CryptoFaces", "CF") { }
+
+    /********************
+     * Basic Operations *
+     ********************/
     /**
      * @dev Internal function to mint a new token.
      * @dev Private (CF Artists only)
@@ -79,19 +118,66 @@ Pausable {
      * Reverts if the given token ID already exists, and if the face already exists
      * @param _tokenURI The Face Hash the will be minted with the token
      */
-    function mint(address _to ,string memory _tokenURI) public onlyIfCryptoFacesArtists {
-        require(!faceExists[_tokenURI]);
+    function mint(address _to ,string memory _tokenURI) public
+    onlyIfCryptoFacesArtists {
+        require(!tokenURIExists[_tokenURI]);
 
         mintWithTokenURI(_to, tokenId, _tokenURI);
 
-        faceExists[_tokenURI] = true;
-        facesURI.push(_tokenURI);
+        tokenURIExists[_tokenURI] = true;
+        tokenURIs.push(_tokenURI);
 
+        // inheriting ti
         emit Minted(tokenId, _tokenURI, _to);
 
         tokenId = tokenId.add(1);
     }
 
+    /**
+     * @dev Public entry point for purchasing a Token
+     * @dev Reverts if payment not provided in full
+     * @dev Reverts if token is sold out
+     * @dev Reverts if token is not active or available
+     */
+    function purchase(uint256 _tokenId)
+    public
+    payable
+    returns (uint256) {
+        return purchaseTo(msg.sender, _tokenId);
+    }
+
+    /**
+     * @dev Public entry point for purchasing an edition on behalf of someone else
+     * @dev Reverts if edition is invalid
+     * @dev Reverts if payment not provided in full
+     * @dev Reverts if edition is sold out
+     * @dev Reverts if edition is not active or available
+     */
+    /*function purchaseTo(address _to, uint256 _tokenId)
+    public
+    payable
+    returns (uint256) {
+
+        require(msg.value >= _editionDetails.priceInWei, "Value must be greater than price of edition");
+
+        // Construct next token ID e.g. 100000 + 1 = ID of 100001 (this first in the edition set)
+        uint256 _tokenId = _nextTokenId(_editionNumber);
+
+        // Create the token
+        _mintToken(_to, _tokenId, _editionNumber, _editionDetails.tokenURI);
+
+        // Splice funds and handle commissions
+        _handleFunds(_editionNumber, _editionDetails.priceInWei, _editionDetails.artistAccount, _editionDetails.artistCommission);
+
+        // Broadcast purchase
+        emit Purchase(_tokenId, _editionNumber, _to, msg.value);
+
+        return _tokenId;
+    }*/
+
+    /***************
+     * Token Query *
+     ***************/
     /**
      * @dev Returns whether the specified token exists.
      * @param _tokenId uint256 ID of the token to query the existence of
@@ -113,5 +199,9 @@ Pausable {
         _tokenIds = super._tokensOfOwner(owner);
 
         return _tokenIds;
+    }
+
+    function burn(uint256 _tokenId) public onlyIfCryptoFacesArtists {
+
     }
 }
