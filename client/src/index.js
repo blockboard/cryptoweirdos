e0da1a1a7276a9c7969ba66c5238e6e487ab9d86
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-02-14 02:43:42 +0000

    Merge with an external React project template, fixes some issues with merging.

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Web3Provider } from 'react-web3';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-
-        <App/>
-
-    , document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
-import React from "react";
-import ReactDOM from "react-dom";
 import { createBrowserHistory } from "history";
 import { Router, Route, Switch } from "react-router-dom";
 
@@ -43,4 +24,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
->>>>>>> react-template/master
