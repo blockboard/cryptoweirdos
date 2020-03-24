c584766b5c9394feb5ef2c581f32a55b15d6c5af
iskanderandrews <eskandr.samir@fci.helwan.edu.eg>
2020-03-24 14:42:33 +0000

    Adding config options

--- app.js
+++ app.js
@@ -55,7 +55,8 @@ app.use((error, req, res, next) => {
 // Connection to db and start server
 mongoose
     .connect(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-mv5jp.mongodb.net/test?retryWrites=true&w=majority`
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-mv5jp.mongodb.net/test?retryWrites=true&w=majority`,
+      { useNewUrlParser: true }
     )
     .then(result => {
         app.listen(process.env.PORT || 5000);
